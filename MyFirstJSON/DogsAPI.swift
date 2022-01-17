409c198bd22094534b4fe41fb1d029e19b150676
DYBEDY <rd.iosdeveloper@gmail.com>
2022-01-17 18:56:48 +0300

    test1

---
+++ MyFirstJSON/DogsAPI.swift
@@ -0,0 +1,15 @@
+//
+//  WikipediaAPI.swift
+//  MyFirstJSON
+//
+//  Created by Roman on 17.01.2022.
+//
+
+import Foundation
+import UIKit
+
+struct DogsAPI: Decodable {
+    let message: String
+    let status: String
+    
+}
--- MyFirstJSON/ViewController.swift
+++ MyFirstJSON/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var newImage: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getJson()
+        
     }
 
-
+    func getJson() {
+        guard let url = URL(string: "https://dog.ceo/api/breeds/image/random") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "NO error")
+                return
+            }
+            
+            do {
+                let wiki = try JSONDecoder().decode(DogsAPI.self, from: data)
+                print(wiki)
+            } catch {
+                print(error.localizedDescription)
+            }
+            guard let image = UIImage(data: data) else { return }
+            self.newImage.image = image
+            
+           
+        } .resume()
+    }
 }
 
